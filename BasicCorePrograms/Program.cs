ebe0d6ec174bc42d6c061b98d1ec3a49e3781070
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-20 23:02:48 +0530

    Welcome to leap year code

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -4,27 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Programs");
-            Random random = new Random();
-            Console.WriteLine("Enter the number of times for coin flip");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int head = 0, tail = 0;
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("Enter the year in YYYY format:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
             {
-                double coinFlipped = random.NextDouble();
-                if (coinFlipped < 0.5)
-                {
-                    tail++;
-                }
-                else
-                {
-                    head++;
-                }
+                Console.WriteLine("{0} is a leap year.", year);
             }
-            double headPercentage = head * 100 / n;
-            Console.WriteLine($"Head won {head} times, Head Score: {headPercentage}%");
-            double tailPercentage = tail * 100 / n;
-            Console.WriteLine($"Tail won {tail} times, Tail Score: {tailPercentage}%");
+            else
+            {
+                Console.WriteLine("{0} is not a leap year.", year);
+            }
         }
     }
 }
